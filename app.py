dd79e1b0c4566d8e7db20007aadfd0eb095ee582
idkwhattonamemyself115 <khu27@nmhschool.org>
2025-09-25 22:38:16 -0400

    game rule+fixing stuctu

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 import random 
+import time 
 user=[]
 bot=[]
 win=False
 dealerstays=False
 print("welcome to blackjack! You start with 2 cards, try to get as close to 21 as possible without going over.")
 for i in range(2):
-    user.append(random.randint(1,14))
-    bot.append(random.randint(1,14))
+    user.append(random.randint(1,13))
+    bot.append(random.randint(1,13))
+    print("your cards:",user)
+    print("dealer's cards:",bot)
 if sum(user)>=21:
     print("your total is ",sum(user),"bot wins")
 while sum(user)<21 and win==False:
-    print("your cards:",user)
-    print("dealer's cards:",bot)
     another_round=input("your total is "+str(sum(user))+", do you want another card? (y/n)")
     if another_round=="y":
-        user.append(random.randint(1,14))
+        user.append(random.randint(1,13))
         print("your cards:",user)
+    if sum(user)>21:
+        print("you lose! your total is ",sum(user))
+        break
     if sum(bot)<sum(user) and sum(bot)<16:
+        print("waiting for dealer's turn...")
+        time.sleep(2)
         print("dealer draws")
-        bot.append(random.randint(1,14))
-        print("dealer's cards:",bot)
-    else: 
+        bot.append(random.randint(1,13))
+        print("dealer's cards:",bot, "with a total of",sum(bot))
+    elif sum(bot)<sum(user):
+        bot.append(random.randint(1,13)) 
+        print("waiting for dealer's turn...")
+        time.sleep(2)
         print("dealer stays")
         dealerstays=True
     if another_round=="n" and dealerstays==True:
@@ -36,9 +45,6 @@ while sum(user)<21 and win==False:
             print("it's a tie! go again")
             dealerstays=False
             another_round="y"
-    if sum(user)>21:
-        print("you lose!")
-        break
     elif sum(bot)>21:
         print("you win!")
         win=True
